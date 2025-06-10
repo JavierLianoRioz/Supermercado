c115bb57ba1186026325c7984f8f3cda3a755a35
midas@tower.windows <javier.liano@alumnos.uneatlantico.es>
2025-06-10 17:28:29 +0200

    Añadir metodo Cliente

---
+++ src/Restaurante.java
@@ -0,0 +1,9 @@
+
+public class Restaurante {
+
+    public void abrir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'abrir'");
+    }
+
+}
